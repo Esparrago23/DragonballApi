13c681be6db3151fb9948ddc3189dd73f42c9e4f
Christian <minisirhc@gmail.com>
2024-11-28 12:31:41 -0600

    First commit

---
+++ src/app/personajes/personajes-transformaciones/personajes-transformaciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes-transformaciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './personajes-transformaciones.component.html',
+  styleUrl: './personajes-transformaciones.component.css'
+})
+export class PersonajesTransformacionesComponent {
+
+}
